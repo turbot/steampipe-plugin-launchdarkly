eb9dfda3074e8177733bb71249e10d89448a5a3c
Madhushree Ray <65847563+madhushreeray30@users.noreply.github.com>
2023-04-25 14:26:58 +0530

    Add table launchdarkly_environment (#6)
    Co-authored-by: misraved <ved@turbot.com>

--- launchdarkly/plugin.go
+++ launchdarkly/plugin.go
@@ -20,9 +20,10 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			"launchdarkly_access_token": tablelaunchdarklyAccessToken(ctx),			
-			"launchdarkly_audit_log":		 tablelaunchdarklyAuditLog(ctx),	
+			"launchdarkly_access_token": tablelaunchdarklyAccessToken(ctx),
+			"launchdarkly_audit_log":		 tablelaunchdarklyAuditLog(ctx),
       "launchdarkly_project":      tablelaunchdarklyProject(ctx),
+			"launchdarkly_environment":  tablelaunchdarklyEnvironment(ctx),
 		},
 	}
 	return p
