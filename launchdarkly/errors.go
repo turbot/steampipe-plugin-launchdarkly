7a33b22150ceb490d179e83b63e41b48af513878
Ved misra <47312748+misraved@users.noreply.github.com>
2023-04-25 16:04:19 +0530

    Add table launchdarkly team (#7)

--- launchdarkly/errors.go
+++ launchdarkly/errors.go
@@ -2,17 +2,17 @@ package launchdarkly
 
 import (
 	"context"
-	"path"
+	"strings"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
-// shouldIgnoreErrors:: function which returns an ErrorPredicate for Aiven API calls
+// shouldIgnoreErrors:: function which returns an ErrorPredicate for LaunchDarkly API calls
 func shouldIgnoreErrors(notFoundErrors []string) plugin.ErrorPredicateWithContext {
 	return func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData, err error) bool {
 		for _, pattern := range notFoundErrors {
 			// handle not found error
-			if ok, _ := path.Match(pattern, "404"); ok {
+			if strings.Contains(err.Error(), pattern) {
 				return true
 			}
 		}
--- launchdarkly/plugin.go
+++ launchdarkly/plugin.go
@@ -20,10 +20,11 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			"launchdarkly_access_token": tablelaunchdarklyAccessToken(ctx),
-			"launchdarkly_audit_log":		 tablelaunchdarklyAuditLog(ctx),
+			"launchdarkly_access_token": tablelaunchdarklyAccessToken(ctx),			
+			"launchdarkly_audit_log":	   tablelaunchdarklyAuditLog(ctx),
+      "launchdarkly_environment":  tablelaunchdarklyEnvironment(ctx),
       "launchdarkly_project":      tablelaunchdarklyProject(ctx),
-			"launchdarkly_environment":  tablelaunchdarklyEnvironment(ctx),
+			"launchdarkly_team":         tablelaunchdarklyTeam(ctx),
 		},
 	}
 	return p
