333b753d2d2e78aec7101496345e9fd047adc0cd
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 17:37:40 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#25)

--- launchdarkly/plugin.go
+++ launchdarkly/plugin.go
@@ -18,7 +18,6 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		DefaultRetryConfig: &plugin.RetryConfig{ShouldRetryErrorFunc: shouldRetryError([]string{"429"})},
 		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
 			NewInstance: ConfigInstance,
-			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
 			"launchdarkly_access_token":   tablelaunchdarklyAccessToken(ctx),
